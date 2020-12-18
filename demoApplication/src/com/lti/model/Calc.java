64011143cf179ebcafa35eb95477dd278490a46d
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-18 16:06:28 +0530

    First Commit

---
+++ demoApplication/src/com/lti/model/Calc.java
@@ -0,0 +1,18 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int num1, int num2){
+		System.out.println("Sum= "+(num1+num2));
+	}
+	public void subs(int num1,int num2){
+		System.out.println("Difference: "+(num1-num2));
+	}
+	public void mul(int num1,int num2){
+		System.out.println("multiply: "+(num1*num2));
+	}
+	
+	public void div(int num1,int num2){
+		System.out.println("multiply: "+(num1/num2));
+	}
+	
+}
---
+++ demoApplication/src/com/lti/model/Sample.java
@@ -0,0 +1,10 @@
+package com.lti.model;
+
+public class Sample {
+	public void show(){
+		System.out.println("Hello world");
+	}
+	public void method(){
+		System.out.println("hi all, how are you doing?");
+	}
+}
