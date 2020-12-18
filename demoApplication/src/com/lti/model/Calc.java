397c3b0fcfe7036045684430ed857d7a7275ae38
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-18 16:32:49 +0530

    removed Sysout part to create conflict

--- demoApplication/src/com/lti/model/Calc.java
+++ demoApplication/src/com/lti/model/Calc.java
@@ -2,7 +2,7 @@ package com.lti.model;
 
 public class Calc {
 	public void add(int num1, int num2){
-		System.out.println("Sum= "+(num1+num2));
+		("Sum= "+(num1+num2));
 	}
 	public void subs(int num1,int num2){
 		System.out.println("Difference: "+(num1-num2));
